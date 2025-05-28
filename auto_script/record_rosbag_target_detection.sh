ed49a35a37cfe75fe52ea541fe14b07ae7af3b4d
YuexinMu <2444780821@qq.com>
2025-05-28 09:40:19 +0800

    Add sh script of recording rosbag target detection.

---
+++ auto_script/record_rosbag_target_detection.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 设置 rosbag 保存路径
+BAG_DIR=/home/orangepi/rosbag2
+BAG_NAME=target_dectection_$(date +%Y%m%d_%H%M%S).bag
+
+mkdir -p "$BAG_DIR"
+
+echo "Start recording rosbag..."
+echo "Save dir: $BAG_DIR/$BAG_NAME"
+echo "Record topics: /ouster/imu /ouster/points /camera/camera_info /camera/image/compressed /tf_static /odom"
+
+# 录制 rosbag（无限时间，手动中断）
+rosbag record -O "$BAG_DIR/$BAG_NAME" /ouster/imu /ouster/points /camera/camera_info /camera/image/compressed /tf_static /odom
